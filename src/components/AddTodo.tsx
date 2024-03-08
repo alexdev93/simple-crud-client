50266d04a259f0bb2d634c9a3aaf1d02de38d425
alemayehu.dev <alemayehu.dev@gmail.com>
2024-03-08 12:08:31 -0800

    fix bugs

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { addTodo } from "../redux/actions";
+import { addTodo, setTodos } from "../redux/actions";
 import { useDispatch } from "react-redux";
 import axios from "axios";
 
